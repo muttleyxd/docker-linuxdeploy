26eac886f9915702c44a52ee50219ead811f22b7
Mateusz Szychowski (Muttley) <mateusz@szychowski.it>
2020-08-11 23:02:34 +0200

    simplifystuff

--- install-linuxdeploy.sh
+++ install-linuxdeploy.sh
@@ -4,8 +4,9 @@ set -ex
 
 source /entrypoint.sh
 
-git clone https://github.com/linuxdeploy/linuxdeploy.git --depth=1 /tmp/linuxdeploy
+git clone https://github.com/linuxdeploy/linuxdeploy.git /tmp/linuxdeploy
 pushd /tmp/linuxdeploy
+git checkout 557bad2241df2c33972c7e6bdbf0c528cee27cc8
 
 git submodule update --init --recursive
 cmake . -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Release -DUSE_SYSTEM_CIMG=Off -DUSE_CCACHE=Off
