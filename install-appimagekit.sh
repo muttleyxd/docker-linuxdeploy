1756c39fe3b9324a355327bd848339905da769cd
Mateusz Szychowski (Muttley) <mateusz@szychowski.it>
2022-09-10 12:01:28 +0200

    lock appimagekit version

--- install-appimagekit.sh
+++ install-appimagekit.sh
@@ -4,8 +4,9 @@ set -ex
 
 source /entrypoint.sh
 
-git clone --recursive https://github.com/AppImage/AppImageKit --depth=1 /tmp/AppImageKit
+git clone --recursive https://github.com/AppImage/AppImageKit /tmp/AppImageKit
 cd /tmp/AppImageKit
+git checkout a2d9cfcb8f662ff8aad5122ce57f2d1898c25980
 
 cmake . -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Release
 make -j`nproc`
