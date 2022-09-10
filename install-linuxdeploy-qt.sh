c6d427c288d087b530b7cf5e47b526a6c7f4e4b2
Mateusz Szychowski (Muttley) <mateusz@szychowski.it>
2022-09-10 12:16:51 +0200

    lock linuxdeploy-qt

--- install-linuxdeploy-qt.sh
+++ install-linuxdeploy-qt.sh
@@ -6,10 +6,11 @@ source /entrypoint.sh
 
 BUILD_DIR=/tmp/linuxdeploy-pluggin-qt
 
-git clone https://github.com/linuxdeploy/linuxdeploy-plugin-qt --depth=1 ${BUILD_DIR}
+git clone https://github.com/linuxdeploy/linuxdeploy-plugin-qt ${BUILD_DIR}
 pushd ${BUILD_DIR}
-
+git checkout 7dcddc5
 git submodule update --init --recursive
+
 cmake . -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Release  -DUSE_SYSTEM_CIMG=Off -DUSE_CCACHE=Off -DCMAKE_CXX_FLAGS="-include map"
 make -j`nproc`
 make install
