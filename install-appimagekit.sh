ba3c1db585d3d3a1c146102b5f1e94e642cedc36
Mateusz Szychowski (Muttley) <mateusz@szychowski.it>
2026-02-24 11:44:48 +0100

    Fix submodule initialization after checkout

--- install-appimagekit.sh
+++ install-appimagekit.sh
@@ -7,6 +7,7 @@ source /entrypoint.sh
 git clone --recursive https://github.com/AppImage/AppImageKit /tmp/AppImageKit
 cd /tmp/AppImageKit
 git checkout a2d9cfcb8f662ff8aad5122ce57f2d1898c25980
+git submodule update --init --recursive
 
 cmake . -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 make -j`nproc`
